da082333a11cacd1542a891e7b03c0972c5c0264
dimakarabutov <91184402+dimakarabutov@users.noreply.github.com>
2021-09-22 12:31:37 +0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,8 +1,8 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+включитьinclude $(CLEAR_VARS)
 
-LOCAL_MODULE := samp
+LOCAL_MODULE := samp 
 LOCAL_LDLIBS := -llog
 
 # samp
@@ -13,7 +13,7 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/util/*.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/game/RW/RenderWare.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/gui/*.cpp)
 
-# vendor
+# vendor включить
 FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/inih/cpp/INIReader.cpp)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/inih/ini.c)
 FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/RakNet/*.cpp)
@@ -24,4 +24,4 @@ FILE_LIST += $(wildcard $(LOCAL_PATH)/vendor/hash/md5.cpp)
 LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_CPPFLAGS := -w -s -fvisibility=hidden -pthread -Wall -fpack-struct=1 -O2 -std=c++14 -fexceptions
-include $(BUILD_SHARED_LIBRARY)
+include $(BUILD_SHARED_LIBRARY) включить
